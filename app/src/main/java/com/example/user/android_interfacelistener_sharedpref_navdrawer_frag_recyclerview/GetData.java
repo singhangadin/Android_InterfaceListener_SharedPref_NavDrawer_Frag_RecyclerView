c78e63276c9caf221298c2d2024d626b6eacc52c
Aman Sharma <amansharma.11@hotmail.com>
2019-02-08 05:13:49 +0530

    0513

---
+++ app/src/main/java/com/example/user/android_interfacelistener_sharedpref_navdrawer_frag_recyclerview/GetData.java
@@ -0,0 +1,29 @@
+package com.example.user.android_interfacelistener_sharedpref_navdrawer_frag_recyclerview;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class GetData extends Fragment {
+
+
+    public GetData() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_get_data, container, false);
+    }
+
+}
--- app/src/main/java/com/example/user/android_interfacelistener_sharedpref_navdrawer_frag_recyclerview/MainActivity.java
+++ app/src/main/java/com/example/user/android_interfacelistener_sharedpref_navdrawer_frag_recyclerview/MainActivity.java
@@ -1,13 +1,93 @@
 package com.example.user.android_interfacelistener_sharedpref_navdrawer_frag_recyclerview;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+    private ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        // drawerToggle = setupDrawerToogle();
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        // drawerLayout.addDrawerListener(drawerToggle);
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        setupDrawerContent(navigationView);
+
+
+//        if(savedInstanceState == null){
+//            getSupportFragmentManager().beginTransaction().replace(R.id.content_frame,new SendData()).commit();
+//            navigationView.setCheckedItem(R.id.send_data);
+//        }
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch ((item.getItemId())){
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView){
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                selectDrawerItem(menuItem);
+                return true;
+            }
+        });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem){
+        switch (menuItem.getItemId()){
+
+
+                case R.id.get_data :
+                getSupportFragmentManager().beginTransaction().replace(R.id.content_frame,new GetData()).commit();
+                break;
+        }
+
+        menuItem.setChecked(true);
+        setTitle(menuItem.getTitle());
+        drawerLayout.closeDrawer(GravityCompat.START);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+
+
 }
